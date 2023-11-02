8b870d02b7f76a811fca4345446d3195f0c11bc9
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2023-11-02 16:08:24 +0700

    ManyToMany added

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,39 @@
 from django.contrib import admin
+from .models import Article, Tag, Scope
 
-from .models import Article
+# from .models import Object, Relationship
+# from django.forms import BaseInlineFormSet
+# from django.core.exceptions import ValidationError
 
+
+# class RelationshipInlineFormset(BaseInlineFormSet):
+#     def clean(self):
+#         for form in self.forms:
+#             # В form.cleaned_data будет словарь с данными
+#             # каждой отдельной формы, которые вы можете проверить
+#             form.cleaned_data
+#             # вызовом исключения ValidationError можно указать админке о наличие ошибки
+#             # таким образом объект не будет сохранен,
+#             # а пользователю выведется соответствующее сообщение об ошибке
+#             raise ValidationError('Тут всегда ошибка')
+#         return super().clean()  # вызываем базовый код переопределяемого метода
+#
+# class RelationshipInline(admin.TabularInline):
+#     model = Relationship
+#     formset = RelationshipInlineFormset
+#
+# @admin.register(Object)
+# class ObjectAdmin(admin.ModelAdmin):
+#     inlines = [RelationshipInline]
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    pass
+#
+@admin.site.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+#
+@admin.site.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -7,10 +7,25 @@ class Article(models.Model):
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    tags = models.ManyToManyField(Tag, through='Scope')
 
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
 
     def __str__(self):
         return self.title
+
+
+# class Tag(models.Model):
+#     name = models.CharField(max_length=100)
+#
+#     def __str__(self):
+#         return self.name
+#
+# class Scope(models.Model):
+#     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+#     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+#     is_main = models.BooleanField(default=False)
