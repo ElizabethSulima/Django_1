b37c979c781d31ee8273dc1eeb4393a761836658
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2023-11-07 20:32:03 +0700

    Catalog changed

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -24,3 +24,18 @@ def catalog(request):
 def phone_detail_view(request, slug):
     phone = get_object_or_404(Phone, slug=slug)
     return render(request, 'phone_detail.html', {'phone': phone})
+
+def catalog(request):
+    sort_param = request.GET.get('sort')
+
+    if sort_param == 'name':
+        phones = Phone.objects.order_by('name')
+    elif sort_param == 'min_price':
+        phones = Phone.objects.order_by('price')
+    elif sort_param == 'max_price':
+        phones = Phone.objects.order_by('-price')
+    else:
+        phones = Phone.objects.all()
+    context = {'phones': phones}
+
+    return render(request, 'catalog.html', context)
