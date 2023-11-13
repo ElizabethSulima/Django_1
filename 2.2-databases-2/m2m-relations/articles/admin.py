47481d3070a465d791b639b4d58a180be68505be
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2023-11-13 19:52:18 +0700

    ManyToMany changed

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,39 +1,33 @@
 from django.contrib import admin
 from .models import Article, Tag, Scope
 
-# from .models import Object, Relationship
-# from django.forms import BaseInlineFormSet
-# from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from django.core.exceptions import ValidationError
 
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        for form in self.forms:
+            cleaned_data = form.cleaned_data
+            if cleaned_data:
+                print("Содержимое cleaned_data для формы:")
+                for key, value in cleaned_data.items():
+                    print(f"{key}: {value}")
+                raise ValidationError('Тут всегда ошибка')
 
-# class RelationshipInlineFormset(BaseInlineFormSet):
-#     def clean(self):
-#         for form in self.forms:
-#             # В form.cleaned_data будет словарь с данными
-#             # каждой отдельной формы, которые вы можете проверить
-#             form.cleaned_data
-#             # вызовом исключения ValidationError можно указать админке о наличие ошибки
-#             # таким образом объект не будет сохранен,
-#             # а пользователю выведется соответствующее сообщение об ошибке
-#             raise ValidationError('Тут всегда ошибка')
-#         return super().clean()  # вызываем базовый код переопределяемого метода
-#
-# class RelationshipInline(admin.TabularInline):
-#     model = Relationship
-#     formset = RelationshipInlineFormset
-#
-# @admin.register(Object)
-# class ObjectAdmin(admin.ModelAdmin):
-#     inlines = [RelationshipInline]
+        return super().clean()
+
+class RelationshipInline(admin.TabularInline):
+    model = Tag
+    formset = RelationshipInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
-#
+    inlines = [RelationshipInline]
+
 @admin.site.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     pass
-#
+
 @admin.site.register(Scope)
 class ScopeAdmin(admin.ModelAdmin):
     pass
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 
-
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
@@ -14,18 +12,27 @@ class Article(models.Model):
         verbose_name_plural = 'Статьи'
         ordering = ['-published_at']
 
-
     def __str__(self):
         return self.title
 
 
-# class Tag(models.Model):
-#     name = models.CharField(max_length=100)
-#
-#     def __str__(self):
-#         return self.name
-#
-# class Scope(models.Model):
-#     article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
-#     tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
-#     is_main = models.BooleanField(default=False)
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='scopes')
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["tag", "is_main"]
+
+    def __str__(self):
+        if self.is_main:
+            return f"{self.tag} (main)"
+        else:
+            return str(self.tag)
