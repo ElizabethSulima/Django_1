d128aef5baba308515ca3aa943b1ecfe594bc4be
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2023-11-21 14:27:11 +0700

    CRUD added

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
-
-from logistic.models import Product, Stock
-from logistic.serializers import ProductSerializer, StockSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
 
 
 class ProductViewSet(ModelViewSet):
@@ -14,3 +14,6 @@ class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
     # при необходимости добавьте параметры фильтрации
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['related_name', ]
+
