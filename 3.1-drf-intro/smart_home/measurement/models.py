d1f4e8657f4d50e617b3af960c119cfe8a72d370
Elizaveta Sulimenko <sulimenko_eliz@mail.ru>
2024-01-13 21:02:24 +0300

    models changed

--- 3.1-drf-intro/smart_home/measurement/models.py
+++ 3.1-drf-intro/smart_home/measurement/models.py
@@ -17,7 +17,7 @@ class Sensor(models.Model):
 
 
 class Measurement(models.Model):
-    sensor = models.ForeignKey(Sensor, related_name=True, on_delete=models.CASCADE)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name="measurements")
     temperature = models.FloatField()
     time = models.DateTimeField(auto_now=True)
 
